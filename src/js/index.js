33b9d9ff335beee39072dc0343562712f714e909
Andy <obrezkov.andy@gmail.com>
2024-08-02 10:31:21 +0300

    Set up form for sending data

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,10 @@ import smoothScroll from './modules/smooth-scroll.js';
 import swiper from './modules/swiper.js';
 import modal from './modules/modal.js';
 import accordion from './modules/accordion.js';
+import web3form from './modules/web3form.js';
 
 burgerMenu();
 smoothScroll();
 modal();
 accordion();
+web3form();
---
+++ src/js/modules/web3form.js
@@ -0,0 +1,38 @@
+function web3form() {
+  const formNode = document.querySelector('#form');
+  const resultNode = document.querySelector('#formResult');
+  const submitButtonNode = document.querySelector('#formSubmit')
+
+  formNode.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formData = new FormData(formNode);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    submitButtonNode.classList.add('none');
+    resultNode.innerHTML = 'Отправка...'
+
+    fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: json
+    })
+      .then(async (response) => {
+        if (response.status == 200) {
+          resultNode.innerHTML = 'Заявка успешно отправлена!';
+        } else {
+          console.log(response);
+          resultNode.innerHTML = 'Что-то не работает... Попробуйте воспользоваться контактами для связи.';
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        resultNode.innerHTML = 'Что-то не работает... Попробуйте воспользоваться контактами для связи.';
+      });
+  });
+}
+
+export default web3form;
