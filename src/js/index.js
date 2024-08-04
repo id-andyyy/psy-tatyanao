7c063dced0e2dc04093efc40fc55c84824eb1e10
Andy <obrezkov.andy@gmail.com>
2024-08-04 17:06:01 +0300

    Add browser compatibility, remove extra SmoothScroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 import burgerMenu from './modules/burger-menu.js';
-import smoothScroll from './modules/smooth-scroll.js';
 import swiper from './modules/swiper.js';
 import modal from './modules/modal.js';
 import accordion from './modules/accordion.js';
@@ -7,7 +6,6 @@ import web3form from './modules/web3form.js';
 import currentYear from './modules/current-year.js';
 
 burgerMenu();
-smoothScroll();
 modal();
 accordion();
 web3form();
